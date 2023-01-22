cee410ee602802ff5b03bd27e8921347ab80ffd8
Malsim Dauhanosav <max_air@bk.ru>
2023-01-22 18:00:18 +0300

    services information

--- dist/js/script.js
+++ dist/js/script.js
@@ -17,27 +17,18 @@ burger.addEventListener("click", () => {
 //accordion
 let servicesItems = document.querySelectorAll(".services__wrapper__item");
 
-
 servicesItems.forEach(item => {
     item.addEventListener("click", e => {
-        console.log(e.target.parentNode.parentNode);
-
-
-        if (e.target.parentNode.parentNode.classList.contains("services__wrapper__item")) {
-            e.target.parentNode.parentNode.classList.add("active_accordion");
+        if (e.target.parentNode.classList.contains("services__wrapper__item-header-active")) {
+            e.target.parentNode.classList.remove("services__wrapper__item-header-active");
+            e.target.parentNode.classList.add("services__wrapper__item-header");
+            e.target.parentNode.nextElementSibling.classList.remove("services__wrapper__item-accordion-active");
+            e.target.parentNode.nextElementSibling.classList.add("services__wrapper__item-accordion");
+        } else {
+            e.target.parentNode.classList.remove("services__wrapper__item-header");
+            e.target.parentNode.classList.add("services__wrapper__item-header-active");
+            e.target.parentNode.nextElementSibling.classList.remove("services__wrapper__item-accordion");
+            e.target.parentNode.nextElementSibling.classList.add("services__wrapper__item-accordion-active");
         }
-
-
-
-
-
-        // if (e.target.parentNode.classList.contains("services__wrapper__item-header") || e.target.parentNode.parentNode.classList.contains("services__wrapper__item-header") || e.target.classList.contains("services__wrapper__item-header") || e.target.parentNode.parentNode.classList.contains("Iconly/Bold/Arrow---Down-Circle")) {
-        //     console.log(e.target.parentNode.parentNode.parentNode.parentNode.parentNode.nextElementSibling);
-        //     e.target.nextElementSibling.style.display = "block";
-        //     e.target.nextElementSibling.style.visibility = "visible";
-        // }
-        // console.log(e.target);
-        // e.classList.add("active_accordion");
-
     });
 });
