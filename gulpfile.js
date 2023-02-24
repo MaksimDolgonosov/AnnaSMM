e7b574eaa3c5dc3623846dff6baad561e156d946
max_air <max_air@bk.ru>
2023-02-24 12:44:12 +0300

    add prod-build-js

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,15 @@
-const gulp        = require('gulp');
+const gulp = require('gulp');
 const browserSync = require('browser-sync');
-const sass        = require('gulp-sass');
+const sass = require('gulp-sass');
 const cleanCSS = require('gulp-clean-css');
 const autoprefixer = require('gulp-autoprefixer');
 const rename = require("gulp-rename");
 const imagemin = require('gulp-imagemin');
 const htmlmin = require('gulp-htmlmin');
+const webpack = require("webpack-stream");
 
-gulp.task('server', function() {
+
+gulp.task('server', function () {
 
     browserSync({
         server: {
@@ -18,17 +20,17 @@ gulp.task('server', function() {
     gulp.watch("src/*.html").on('change', browserSync.reload);
 });
 
-gulp.task('styles', function() {
+gulp.task('styles', function () {
     return gulp.src("src/sass/**/*.+(scss|sass)")
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(rename({suffix: '.min', prefix: ''}))
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+        .pipe(rename({ suffix: '.min', prefix: '' }))
         .pipe(autoprefixer())
-        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(cleanCSS({ compatibility: 'ie8' }))
         .pipe(gulp.dest("dist/css"))
         .pipe(browserSync.stream());
 });
 
-gulp.task('watch', function() {
+gulp.task('watch', function () {
     gulp.watch("src/sass/**/*.+(scss|sass|css)", gulp.parallel('styles'));
     gulp.watch("src/*.html").on('change', gulp.parallel('html'));
     gulp.watch("src/js/**/*.js").on('change', gulp.parallel('scripts'));
@@ -68,4 +70,23 @@ gulp.task('images', function () {
         .pipe(browserSync.stream());
 });
 
+gulp.task("build-prod-js", () => {
+    return gulp.src("./src/js/script.js")
+        .pipe(webpack({
+            mode: 'production',
+            output: {
+                filename: 'script.js'
+            },
+            module: {
+                rules: [
+                    {
+                        // test: /\.m?js$/,
+                        exclude: /(node_modules|bower_components)/,
+
+                    }
+                ]
+            }
+        }))
+        .pipe(gulp.dest("./dist/js/"));
+});
 gulp.task('default', gulp.parallel('watch', 'server', 'styles', 'scripts', 'fonts', 'icons', 'html', 'images'));
